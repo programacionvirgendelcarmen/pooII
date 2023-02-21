ed0c43d00aecccf2cc80315898a166edb2f368a2
manuel <programacionvirgendelcarmen@gmail.com>
2023-02-21 14:20:01 +0100

    Composición, ejemplo agenda, completar TestAgenda, enum, JOptionPane

--- src/teoria/composicion/TestAgenda.java
+++ src/teoria/composicion/TestAgenda.java
@@ -1,5 +1,7 @@
 package teoria.composicion;
 
+import strings.StringHelper;
+
 import javax.swing.*;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,11 +12,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public class TestAgenda {
+    private static Agenda agenda;
     public static void main(String[] args) throws IOException {
         List<Contacto> contactos = new ArrayList<>();
         List<String> lineas = Files.readAllLines(
                 Paths.get("datos/contactos.csv"));
-        System.out.printf("Leidas %d líneas%n", lineas.size());
+       // System.out.printf("Leidas %d líneas%n", lineas.size());
         for (String linea : lineas) {
             String[] tokens = linea.split(",");
             String nombreContacto = tokens[0];
@@ -27,21 +30,65 @@ public class TestAgenda {
             contactos.add(contacto);
         }
         //comprobación de los contactos:
-        contactos.forEach(System.out::println);
-        System.out.printf("%nNº contactos %d%n", contactos.size());
+       // contactos.forEach(System.out::println);
+       // System.out.printf("%nNº contactos %d%n", contactos.size());
         String[] nombresAgenda = { "TRABAJO", "AMIGOS", "GIMNASIO", "FAMILIA" };
         Object selectedValue = JOptionPane.showInputDialog(null,
-                "Choose one", "Input",
+                "Elige tipo de agena", "AGENDA",
                 JOptionPane.INFORMATION_MESSAGE, null,
                 nombresAgenda, nombresAgenda[0]);
         String sValor = (String) selectedValue;
         NombreAgenda nombreAgenda = NombreAgenda.valueOf(sValor);
-        Agenda agenda = new Agenda(nombreAgenda, contactos);
+
+        agenda = new Agenda(nombreAgenda, contactos);
+        desplegarMenu();
         //Usando JOption menú con dos opciones:
         //1. añadir contacto
         //2. mostrar contacto
         //para cada un un método, ejemplo mostrarContacto(Contacto contacto)
         //usando JOptionPane mostramos el contacto
         //añadir contacto, pedimos cuatro datos
+    }
+
+    private static void desplegarMenu() {
+        String opcion = "";
+        do {
+            opcion =
+                    JOptionPane.showInputDialog(
+                            "Introduce una opción\n1. Añadir contacto.\n2. Mostrar contacto\n3. Salir");
+        } while (! opcion.matches("[12]"));
+        //System.out.println("opción elegida " + opcion);
+        switch (opcion) {
+            case "1" :
+                addContacto();
+                break;
+            case "2" :
+                mostrarContactoPorTelefono();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private static void mostrarContactoPorTelefono() {
+        String telefono = "";
+        do {
+            telefono =
+                    JOptionPane.showInputDialog(
+                            "Introduce nº teléfono del contacto a localizar");
+        } while (!StringHelper.checkPhoneNumber(telefono));
+        Contacto contacto = agenda.mostrarContacto(telefono);
+        if (contacto != null)
+            JOptionPane.showMessageDialog(
+                    null, contacto, "CONTACTO",
+                    JOptionPane.INFORMATION_MESSAGE);
+        else
+            JOptionPane.showMessageDialog(
+                    null, "No existe el contacto", "CONTACTO",
+                    JOptionPane.ERROR_MESSAGE);
+    }
+
+    private static void addContacto() {
+        agenda.mostrarContacto("telefono");
     }
 }
